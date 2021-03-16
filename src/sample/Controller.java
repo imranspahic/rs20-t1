5eb6dd38e0447a813bb3775d4259452296f217db
imranspahic <62642796+imranspahic@users.noreply.github.com>
2021-03-16 15:16:34 +0100

    alignment u vboxu, i css klase izmjene za prikaz i sakrivanje labele

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,8 @@ public class Controller {
     public GridPane prozor;
 
     public void setText(ActionEvent actionEvent) {
+        helloWorld.getStyleClass().removeAll("sakriveno");
+        helloWorld.getStyleClass().add("prikazano");
         helloWorld.setText("Hello World!");
         prozor.setStyle("-fx-background-color: lightpink");
     }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello ss");
-        primaryStage.setScene(new Scene(root, 300, 275));
-
+        primaryStage.setTitle("Tutorijal 1");
+        primaryStage.setScene(new Scene(root, 400, 275));
         primaryStage.show();
     }
 
